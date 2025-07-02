3d2bb787b06270d48e32c1d3449e8411e993baaa
Emrullah Yavuz <85703039+emrullahyavuz@users.noreply.github.com>
2025-07-02 16:13:27 +0300

    feat: add dotenv and cors support, restructure server files

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const app = express();
+require("dotenv").config();
+const app = require("./app.js");
+const PORT = process.env.PORT || 3000;
 
-const PORT = 3000;
 
 app.get("/api/test", (req, res) => {
     res.send("Welcome to the Health Appointment API");
