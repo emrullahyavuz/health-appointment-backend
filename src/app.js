f215597d65f9b1afcc8ea39ba44363167197e357
Emrullah Yavuz <85703039+emrullahyavuz@users.noreply.github.com>
2025-07-03 08:52:10 +0300

    feat: configure CORS options for enhanced security and flexibility

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const corsOptions = require("./config/cors.config.js");
 const authRoutes = require("./routes/authRoutes.js");
 const userRoutes = require("./routes/userRoutes.js");
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Routes
